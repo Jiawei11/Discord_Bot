b7f040f318939eb85b1b7683b4f4d9deb6f4853b
jiawei11 <zxc37984@gmail.com>
2019-07-04 10:42:29 +0800

    nodemon command

--- app.js
+++ app.js
@@ -7,22 +7,19 @@ bot.on('ready', () => {
     console.log('Discord Bot Ready');
 })
 
-bot.on('message', msg => {
-    if (msg.content == "嗨") {
-        msg.channel.sendMessage('嗨');
-    }
-})
-
 bot.on('message', message => {
     if (message.author.bot || !message.content.startsWith(prefix)) {
         return;
     } else {
-        switch (message.content.split('?').filter(word => word != '')[0]) {
+        var command = message.content.split('?').filter(word => word != '')[0];
+
+
+        switch (command) {
             case 'ping':
                 message.channel.sendMessage('伺服器延遲為： `' + `${Date.now() - message.createdTimestamp}` + ' ms`');
                 break;
             case 'voice':
-                message.channel.sendMessage('Voice');
+                message.channel.sendMessage('voice ready');
                 break;
             default:
                 message.channel.sendMessage('Default Message');
